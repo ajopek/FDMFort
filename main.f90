ffd1edaf7f6876b657e11f8f788d6e6180fd056c
ajopek <arjopek@gmail.com>
2018-04-28 22:06:13 +0200

    Fix coefficient and values matrix generation

--- main.f90
+++ main.f90
@@ -6,48 +6,36 @@ contains
     subroutine generate_coeffitients(n, coef_matrix)
         implicit none
         integer, intent(in) :: n
-        real (kind = 4), dimension(n - 1, 3), intent(out) :: coef_matrix
-        real (kind = 4) :: h
+        real (kind = 8), dimension(n - 1, 3), intent(out) :: coef_matrix
         integer :: i, j
-        real (kind = 4) :: c1, c2, c3
-
-        ! Calculate points distance
-        h =  1. / n;
+        real (kind = 8) :: c1, c2
 
         ! Calculate coefficients
-        c1 = 1. / (h**2)
-        c2 = -2. / (h**2)
-        c3 = c1
+        c1 = real(n, kind = 8) ** 2
+        c2 = -2.0 * c1
 
-        write (*, *) c1
-        write (*, *) c2
-        write (*, *) c3
 
-        coef_matrix(:, :) = 0
+        coef_matrix(:, :) = real(0, kind = 8)
 
-        coef_matrix(1, 2) = c1
         coef_matrix(1, 1) = c2
+        coef_matrix(1, 2) = c1
 
         do i = 2, (n - 2)
-            coef_matrix(i, 1) = c3
-            coef_matrix(i, 2) = c2
-            coef_matrix(i, 3) = c1
+            coef_matrix(i - 1, i) = c1
+            coef_matrix(i , i) = c2
+            coef_matrix(i + 1, i) = c1
         end do
 
-        coef_matrix(n - 1, 2) = c2
-        coef_matrix(n - 1, 1) = c3
-
+        coef_matrix(n - 1, n - 1) = real(1, kind = 8)
 
     end subroutine generate_coeffitients
 
     subroutine generate_values(n, val_matrix)
         integer, intent(in) :: n
-        real (kind = 4), dimension(n - 1), intent(out) :: val_matrix
+        real (kind = 8), dimension(n - 1), intent(out) :: val_matrix
 
         val_matrix(:) = 0
-        val_matrix(n-1) = -1. / (1. / n)**2
-
-        write (*, *) val_matrix(n-1)
+        val_matrix(n - 1) = 1
 
     end subroutine generate_values
 end module equations_generator
@@ -59,8 +47,8 @@ contains
     subroutine apply_elimination(coef_matrix, val_matrix, matrix_dim)
         implicit none
         integer, intent(in) :: matrix_dim
-        real (kind = 4), dimension(matrix_dim, matrix_dim) :: coef_matrix
-        real (kind = 4), dimension(matrix_dim) :: val_matrix
+        real (kind = 8), dimension(matrix_dim, matrix_dim) :: coef_matrix
+        real (kind = 8), dimension(matrix_dim) :: val_matrix
 
         integer :: i, j
         real :: multiplier
@@ -70,8 +58,9 @@ contains
                     multiplier = coef_matrix(i, j) / coef_matrix(i, i)
                     coef_matrix(:, j) = coef_matrix(:, j) - multiplier * coef_matrix(:, i)
                     val_matrix(j) = val_matrix(j) - multiplier * val_matrix(i)
-                    val_matrix(i) = val_matrix(i) / coef_matrix(i, i)
                 end if
+                val_matrix(i) = val_matrix(i) / coef_matrix(i, i)
+                coef_matrix(:, i) = coef_matrix(:, i) / coef_matrix(i, i)
             end do
         end do
 
@@ -84,18 +73,26 @@ program test
     implicit none
 
     integer (kind = 4) :: N
-    real (kind = 4), allocatable :: coef_matrix(:, :), val_matrix(:)
+    real (kind = 8), allocatable :: coef_matrix(:, :), val_matrix(:)
 
     read(*, *) N
 
-    allocate(coef_matrix(N - 1, 3))
-    allocate(val_matrix(N - 1))
+    allocate(coef_matrix(1:N - 1, 1:N - 1))
+    allocate(val_matrix(1:N - 1))
 
     call generate_coeffitients(N, coef_matrix)
     call generate_values(N, val_matrix)
 
+    write(*, *) val_matrix
+    write(*, *) coef_matrix
+
+    write(*, *) "n"
+
     call apply_elimination(coef_matrix, val_matrix, N - 1)
 
     write(*, *) val_matrix
+
+    deallocate(coef_matrix)
+    deallocate(val_matrix)
 
 end program test
