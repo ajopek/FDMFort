d15dad818804c4fa72c5827d4e6a2877d06a9d36
ajopek <arjopek@gmail.com>
2018-04-29 16:59:26 +0200

    Fix error calculation

--- src/main.f90
+++ src/main.f90
@@ -7,23 +7,36 @@ program main
 
     integer (kind = 4) :: N
     real (kind = iPRECISION), allocatable :: coef_matrix(:, :), val_matrix(:)
+    real(kind = 16), allocatable, dimension(:) :: Ideal, Error
+    integer :: i
 
     read(*, *) N
 
     allocate(coef_matrix(1:N - 1, 1:N - 1))
     allocate(val_matrix(1:N - 1))
+    allocate(Ideal(1:N-1))
+    allocate(Error(1:N-1))
 
     call generate_coeffitients(N, coef_matrix)
     call generate_values(N, val_matrix)
 
-    write(*, *) val_matrix
-    write(*, *) coef_matrix
-
-    write(*, *) "n"
-
     call apply_elimination(coef_matrix, val_matrix, N - 1)
 
-    write(*, *) val_matrix
+    ! Calculate errors
+    ! Sollution to this equation is function f(x) = x
+    Ideal(1) = real(0, kind = 16)
+
+    do i = 2, N-2
+        Ideal(i) = (real(1, kind=16) / real(N - 1, kind=16)) * i
+    end do
+
+    Ideal(N - 1) = real(1, kind = 16)
+
+    Error = val_matrix - Ideal
+
+    print *, iPrecision
+    print *, (SUM(ABS(Error)) / size(Error))
+
 
     deallocate(coef_matrix)
     deallocate(val_matrix)
